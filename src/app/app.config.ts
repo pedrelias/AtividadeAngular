43344576d8d6f09b5e354299f16fa3cf59aae40b
pedrelias <pedreliasz224@gmail.com>
2024-08-19 21:46:37 -0300

    commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { DisplayComponent } from './display/display.component';
 import { KeypadComponent } from './keypad/keypad.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    CalculatorComponent,
-    DisplayComponent,
-    KeypadComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true}), provideRouter(routes)]
+
+}
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     CalculatorComponent,
+//     DisplayComponent,
+//     KeypadComponent
+//   ],
+//   imports: [
+//     BrowserModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
